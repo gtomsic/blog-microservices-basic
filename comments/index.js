26d4e47156a848cee2df780003fcf109462b0402
Gabriel Tomsic <gabriel@macmax.local>
2022-09-17 16:58:47 -0600

    Adding react event-bus and query

--- comments/index.js
+++ comments/index.js
@@ -1,23 +1,33 @@
 const express = require('express');
 const cors = require('cors');
 const { randomBytes } = require('crypto');
+const axios = require('axios');
 
 const commentsByPostId = {};
 
 const app = express();
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
 app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
   const commentId = randomBytes(4).toString('hex');
   const { content } = req.body;
   const comments = commentsByPostId[req.params.id] || [];
   comments.push({ id: commentId, content });
   commentsByPostId[req.params.id] = comments;
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: { id: commentId, content, postId: req.params.id },
+  });
   res.status(201).send(comments);
 });
 
-app.listen(4001, () => console.log('Comments service running on port 4000'));
+app.post('/events', (req, res) => {
+  console.log('Received events', req.body);
+  res.send({});
+});
+
+app.listen(4001, () => console.log('Comments service running on port 4001'));
